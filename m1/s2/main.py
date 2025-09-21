1e33e7e342b56a4fd7fb7cde6260d341793af9e8
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-09-21 10:21:11 +0700

    end of s2

---
+++ m1/s2/main.py
@@ -0,0 +1,13 @@
+def main():
+    x = 10
+    print(x)
+
+# PI_VALUE = 3.14
+# PI_VALUE, GRAVITIY, xxx = 3.14, 9.8, 00.12
+
+# def keliling_lingkaran(r: int):
+#     return 2 * PI_VALUE * r
+
+if __name__ == "__main__":
+    # print(keliling_lingkaran(4))
+    main()
---
+++ m1/s2/test.py
@@ -0,0 +1,6 @@
+val1 = "1"
+val2 = "2"
+val3 = "a"
+val4: int = 6
+print(int(val1) + int(val2) + int(val3))
+print(str(val4))
