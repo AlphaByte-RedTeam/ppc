52323790dbe5dd9d9e1b082d44fdc5b25c118457
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-09-21 10:26:47 +0700

    modify task 1

--- independent_challenge/task1.py
+++
@@ -1,3 +0,0 @@
-# Task: Introduce yourself with variables
-# Create variables: name, age, hobby.
-# Print: "Hi, my name is ___, I am ___ years old, and I like ___."
---
+++ independent_challenge/task1/main.py
@@ -0,0 +1,10 @@
+# Introduce yourself with variables
+# Create variables: name, age, hobby.
+# Print: "Hi, my name is ___, I am ___ years old, and I like ___."
+
+def main():
+    print("Hello from task1!")
+
+
+if __name__ == "__main__":
+    main()
