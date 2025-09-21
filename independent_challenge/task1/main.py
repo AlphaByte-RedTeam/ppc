a3ec76494a6d92b95045e8efcde1fc38aed3d229
James Colin <jamescoling1023@gmail.com>
2025-09-20 23:55:56 -0400

    finished task1

--- independent_challenge/task1/main.py
+++ independent_challenge/task1/main.py
@@ -2,9 +2,11 @@
 # Create variables: name, age, hobby.
 # Print: "Hi, my name is ___, I am ___ years old, and I like ___."
 
-def main():
-    print("Hello from task1!")
-
-
-if __name__ == "__main__":
-    main()
+name = "james"
+print(name) 
+age = int (24)
+print(age)
+hobby = "playing guitar"
+print(hobby) 
+intro = f"hi, my name is {name}, i am {age} years old, and i like {hobby}"
+print(intro)
