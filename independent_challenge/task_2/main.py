20beb3e1c2aa489cdb0ae7e29ee7b7c2e11ad259
James Colin <jamescoling1023@gmail.com>
2025-09-23 16:34:49 -0500

    initial commit

---
+++ independent_challenge/task_2/main.py
@@ -0,0 +1,25 @@
+#Introduce yourself with variables
+
+name = "james" 
+age = 24
+hobby ="i like to playing guitar"
+print (f"Hi, my name is {name} I am {age} years old and {hobby}")
+#Area calculator
+lenght = 10
+width = 5 
+calculated_area = lenght*width 
+print (f"the calculated area is {calculated_area}")
+#swap type
+a = "5"
+b = "10"
+print (str(a))
+print (str(b))
+# final balance 
+balance = 1000
+withdraw = 250 
+deposit = 400 
+final_balance = balance-withdraw+deposit
+print (final_balance)
+#favorite Number 
+favorite_number = 10 
+print (f"my favorite number is {favorite_number}")
