ebce45bdc97b35fe7e40c089cff69522fd10d890
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-10-25 15:54:57 +0700

    update s8

--- m1/s7/main.py
+++ m1/s7/main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from s7!")
+groceries = set(
+    tuple(
+        [
+            "apple",
+            "apple",
+            "apple",
+            "apple",
+            "banana",
+            "milk",
+            "flour",
+            "baking soda",
+        ]
+    )
+)
 
+# print(groceries[0])  # apple
+# groceries[0] = "orange"  # modifying an array
 
-if __name__ == "__main__":
-    main()
+# print(groceries[0])  # orange
+print(groceries)
+groceries.add("orange")
+print(groceries)
+
+# karakteristik list vs tuple
+# list = mutable (artinya value bisa diubah)
+# tuple = immutable (artinya value tidak bisa diubah)
+#
+# set = mutable (artinya value bisa diubah), set tidak berurutan
+# frozenset = immutable (artinya value tidak bisa diubah)
