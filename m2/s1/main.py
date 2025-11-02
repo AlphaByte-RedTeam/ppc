a3b8b967b83eb54607721d56cfbacf4080c55640
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-11-02 09:51:27 +0700

    update s1 m2

--- m2/s1/main.py
+++ m2/s1/main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from s1!")
+def max_number(arr: list[int]) -> int:
+    # pass
+    current_max_number = 0
+    for num in arr:
+        if num > current_max_number:
+            current_max_number = num
+    return current_max_number
 
 
-if __name__ == "__main__":
-    main()
+def min_number(arr: list[int]):
+    # pass
+    current_min_number = float("inf")
+    for num in arr:
+        if num < current_min_number:
+            current_min_number = num
+    return current_min_number
+
+
+array = [89, 23, 100, 434, 12]
+# max_num = max_number(array)
+min_num = min_number(array)
+# print(f"Maximum number from array is {max_num}")
+print(f"Minimum number from array is {min_num}")
